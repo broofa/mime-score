a7bc9363d4a040a0e876b9e7cdc3e12dcd7b03e7
Robert Kieffer <robert@broofa.com>
2023-12-17 07:20:30 -0800

    chore: gitignore dist

--- dist/cli.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-import mimeScore from './mimeScore.js';
-const [, , type, source] = process.argv;
-const score = mimeScore(type, source);
-console.log(score);
--- dist/mimeScore.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const FACET_SCORES: Record<string, number>;
-declare const SOURCE_SCORES: Record<string, number>;
-/**
- * Get each component of the score for a mime type.  The sum of these is the
- * total score.  The higher the score, the more "official" the type.
- */
-export default function (mimeType: string, source?: keyof typeof SOURCE_SCORES): number;
-export {};
